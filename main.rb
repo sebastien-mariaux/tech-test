793e101f6d65207cb83102d07524a00ccc4d3ab3
¨Sebastien <sebastien.mariaux@pm.me>
2025-02-10 15:00:16 +0100

    Rename variables and lint

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 require_relative 'services/purchase_processor'
 
-puts "Enter the path to the file containing the purchase data:"
+puts 'Enter the path to the file containing the purchase data:'
 file_path = gets.chomp
 
 purchase_processor = PurchaseProcessor.new(file_path)
 results = purchase_processor.call
 results.each do |result|
   puts result
-  puts "--------------------------------"
+  puts '--------------------------------'
 end
--- services/purchase_processor.rb
+++ services/purchase_processor.rb
@@ -3,6 +3,7 @@
 require 'json'
 require_relative '../models/item'
 require_relative '../services/receipt_generator'
+
 # Accepts a file path to a JSON file containing purchase data and returns an array of receipts
 class PurchaseProcessor
   def initialize(file_path)
@@ -11,22 +12,22 @@ class PurchaseProcessor
 
   def call
     input = JSON.parse(File.read(@file_path))
-    input['data'].map do |order_data|
-      process_order(order_data)
+    input['data'].map do |shopping_basket|
+      process_basket(shopping_basket)
     end
   end
 
   private
 
-  def process_order(order_data)
-    ReceiptGenerator.new(formatted_order(order_data)).call
+  def process_basket(shopping_basket)
+    ReceiptGenerator.new(formatted_order(shopping_basket)).call
   end
 
-  def formatted_order(order_data)
-    order_data['items'].each_with_object({}) do |item, acc|
-      item_object = Item.new(name: item['name'], price: item['price'], imported: item['imported'],
+  def formatted_order(shopping_basket)
+    shopping_basket['items'].each_with_object({}) do |item, acc|
+      basket_item = Item.new(name: item['name'], price: item['price'], imported: item['imported'],
                              tax_exempt: item['tax_exempt'])
-      acc[item_object] = item['quantity']
+      acc[basket_item] = item['quantity']
     end
   end
 end
