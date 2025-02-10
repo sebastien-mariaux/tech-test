35cf55f62ac175ec1b3db9b9552f736c97e8487c
¨Sebastien <sebastien.mariaux@pm.me>
2025-02-10 14:06:35 +0100

    Add proper formatting

--- helpers.rb
+++ helpers.rb
@@ -1,2 +1,3 @@
 def format_price(price)
+  format('%.2f', price).to_s
 end
--- spec/helpers_spec.rb
+++ spec/helpers_spec.rb
@@ -10,5 +10,11 @@ RSpec.describe 'Helpers' do
     expect(format_price(12.4)).to eq('12.40')
   end
 
+  it 'formats the price with no decimal' do
+    expect(format_price(12)).to eq('12.00')
+  end
 
+  it 'returns a string' do
+    expect(format_price(12.491239480)).to be_a(String)
+  end
 end
