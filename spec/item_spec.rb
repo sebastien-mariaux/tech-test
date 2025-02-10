72e9e6bb84d11e01b60b5c46793e808111494899
¨Sebastien <sebastien.mariaux@pm.me>
2025-02-10 13:43:32 +0100

    Add specs for items tax

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -11,4 +11,42 @@ RSpec.describe 'Item' do
     expect(item.imported).to eq(false)
     expect(item.tax_exempt).to eq(true)
   end
+
+  context 'with an item not imported and tax exempt' do
+    let(:item) { Item.new(name: 'book', price: 12.49, imported: false, tax_exempt: true) }
+
+    it 'returns 0 tax' do
+      expect(item.tax).to eq(0)
+    end
+  end
+
+  context 'with an item imported and tax exempt' do
+    let(:item) { Item.new(name: 'book', price: 12.49, imported: true, tax_exempt: true) }
+
+    let(:expected_tax) { 0.62 }
+
+    it 'applies 5% tax' do
+      expect(item.tax).to eq(0.62)
+    end
+  end
+
+  context 'with an item not imported and not tax exempt' do
+    let(:item) { Item.new(name: 'music cd', price: 14.99, imported: false, tax_exempt: false) }
+
+    let(:expected_tax) { 1.50 }
+
+    it 'applies 10% tax' do
+      expect(item.tax).to eq(1.50)
+    end
+  end
+
+  context 'with an item imported and not tax exempt' do
+    let(:item) { Item.new(name: 'imported bottle of perfume', price: 47.50, imported: true, tax_exempt: false) }
+
+    let(:expected_tax) { 7.15 }
+
+    it 'applies 15% tax' do
+      expect(item.tax).to eq(7.15)
+    end
+  end
 end
